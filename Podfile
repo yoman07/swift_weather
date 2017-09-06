fd1bd36f63e1e6e829ad990737d375dc033386b2
yoman07 <yoman@Romans-MacBook-Pro.local>
2017-09-06 18:44:17 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+target 'Swift Weather' do
+  use_frameworks!
+  pod 'SwiftLint', '~> 0.18.1' 
+  pod 'Swinject', '~> 2.1.0'
+  pod 'SwinjectStoryboard', '~> 1.1.1'
+  pod 'Alamofire', '~> 4.4.0'
+  pod 'Marshal', '~> 1.2.4'
+
+  target 'Swift WeatherTests' do
+    inherit! :search_paths
+    pod 'Quick', '~> 1.1.0'
+    pod 'Nimble', '~> 7.0.1'
+  end
+end
---
+++ Swift Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Weather
+//
+//  Created by Roman Barzyczak on 06/09/2017.
+//  Copyright © 2017 Roman Barzyczak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
